dd3c34488195aefe4ca688c169aba31873cb0d82
gmono <gaozijian0411@foxmail.com>
2021-01-07 15:12:28 +0800

    转移目录

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,6 @@
-import type {SNumToBin,LogicToBin,BinToLogic,BinToSNum,sMul,sEqual,If,Zero,sDec, sDiv, LogicToSNum, Dec} from "."
+
+
+import type {SNumToBin,LogicToBin,BinToLogic,BinToSNum,sMul,sEqual,If,Zero,sDec, sDiv, LogicToSNum, Dec,sSub} from "."
 // import { BinTransToLogic, sMul, sEqual, Zero, sDec, LogicToSNum } from './math';
 import { Equal } from './logic';
 
@@ -12,9 +14,10 @@ type sss=BinToLogic<"11111101">
 type sssss=LogicToSNum<sss>;
 //此处1
 
-type ss=SNumToBin<sDiv<1256,122>>
-type sts=sEqual<ss,2>
-type ssskk=Dec<7777>
+type ss=SNumToBin<sDiv<Dec<1256>,Dec<122>>>
+type sts=sEqual<ss,Dec<2>>
+type ssskk = Dec<7777>
+type ret = If<sEqual<Dec<5>, Dec<2>>, ssskk, sSub<ssskk, Dec<2>>>;
 class test<A extends string,H=(sEqual<A,Zero> extends [false]? test<sDec<A>>:void)>{
     get test():H{
         return null as unknown as  H;
